31a80241f1bc2bc5d237909b8c3265898970ae59
Gonçalo Matos <goncalo@ushift-alfa.tail76ea6.ts.net>
2025-05-27 11:47:16 +0100

    #6 query mobility database method refactored to fix error when filtering by coordinates, now accepting bounding box for simpler interface

--- R/query_mobilitydatabase.R
+++ R/query_mobilitydatabase.R
@@ -8,6 +8,7 @@
 #' @param country_code. String. (Optional) Filter feeds by their exact country code.
 #' @param subdivision_name. String. (Optional) List only feeds with the specified value. Can be a partial match.
 #' @param municipality String. (Optional) List only feeds with the specified value. Can be a partial match. Case insensitive.
+#' @param bbox bbox (Optional) Area from which to get GTFS feeds. Converted to API dataset_latitudes and dataset_longitudes URL parameters.
 #' @param dataset_latitudes Double[]. (Optional) Minimum and maximum latitudes of the bounding box to use for filtering.
 #' @param dataset_longitudes Double[]. (Optional) Minimum and maximum longitudes of the bounding box to use for filtering.
 #' @param is_official. Boolean. (Optional) If true, only return official feeds.
@@ -17,7 +18,7 @@
 #' For more details on the parameters, refer to \url{https://mobilitydata.github.io/mobility-feed-api/SwaggerUI/index.html#/feeds/getGtfsFeeds}.\cr\cr
 #' Some useful columns of the returned data.frame (refer to the API documentation for a full list) are:
 #' \itemize{
-#'  \item `Provider`. The name of the GTFS provider.
+#'  \item `provider`. The name of the GTFS provider.
 #'  \item `status`. Tells if the feed is active, inactive or deprecated.
 #'  \item `producer_url`. The GTFS feed URL. Can be used to download.
 #' }
@@ -42,8 +43,7 @@ query_mobilitydatabase <- function(
     country_code=NA,
     subdivision_name=NA,
     municipality=NA,
-    dataset_latitudes=NA,
-    dataset_longitudes=NA,
+    bbox=NA,
     is_official=NA
 ) {
 
@@ -57,8 +57,10 @@ query_mobilitydatabase <- function(
   if (!is.na(country_code)) params["country_code"] = country_code
   if (!is.na(subdivision_name)) params["subdivision_name"] = subdivision_name
   if (!is.na(municipality)) params["municipality"] = municipality
-  if (!is.na(dataset_latitudes)) params["dataset_latitudes"] = dataset_latitudes
-  if (!is.na(dataset_longitudes)) params["dataset_longitudes"] = dataset_longitudes
+  if (!is.na(bbox)) {
+    params["dataset_latitudes"] = sprintf("%f,%f", bbox$ymin[[1]], bbox$ymax[[1]])
+    params["dataset_longitudes"] = sprintf("%f,%f", bbox$xmin[[1]], bbox$xmax[[1]])
+  }
   if (!is.na(is_official)) params["is_official"] = is_official
 
   response <- GET(
