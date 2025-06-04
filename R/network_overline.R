96d313712a1e5f380a5675c961701262df70e638
RFlx <temospena@gmail.com>
2025-06-04 15:37:54 +0100

    consistency in network_overline

--- R/network_overline.R
+++ R/network_overline.R
@@ -16,15 +16,14 @@
 #' A \code{segment} column will be also added to the network, as a unique identifier used for the aggregation process.
 #'
 #' @examples
-#' \dontrun{
-#' network = st_read("network_centerlines.gpkg")
-#' frequency_analysis <- GTFShift::get_route_frequency_hourly(gtfs)
-#' GTFShift::network_overline(
-#'   network, f
-#'   requency_analysis %>% filter(arrival_hour==8),
-#'   attr = "frequency"
-#' )
-#' }
+# \dontrun{
+#   network = st_read("network_centerlines.gpkg")
+#   frequency_analysis = GTFShift::get_route_frequency_hourly(gtfs)
+#   GTFShift::network_overline(network,
+#                              f
+#                              requency_analysis |> filter(arrival_hour == 8),
+#                              attr = "frequency")
+# }
 #'
 #' @seealso [stplanr::rnet_join]
 #'
@@ -33,35 +32,55 @@
 #' @import dplyr
 #'
 #' @export
-network_overline <- function(network, df, attr, network_segment_length=100, fun=sum, join_dist=10) {
+
+network_overline = function(network,
+                            df,
+                            attr,
+                            network_segment_length = 100,
+                            fun = sum,
+                            join_dist = 10) {
   # 1. Prepare network
-  network_line = stplanr::line_cast(st_transform(network, crs=3857))
+  network_line = stplanr::line_cast(st_transform(network, crs = 3857))
+
   if (!is.na(network_segment_length)) {
-    network_segmented = stplanr::line_segment(network_line, segment_length=network_segment_length) %>% mutate(segment=row_number())
+    network_segmented = stplanr::line_segment(l = network_line,
+                                              segment_length = network_segment_length) |>
+      mutate(segment = row_number())
   } else {
-    network_segmented = network_line %>% mutate(segment=row_number())
+    network_segmented = network_line |>
+      mutate(segment = row_number())
   }
 
-  df = df %>% st_transform(crs=3857) %>% mutate(df_id=row_number())
+  df = df |>
+    st_transform(crs = 3857) |>
+    mutate(df_id = row_number())
 
   # 2. Overlap df and network segments
-  df_network_match = rnet_join(rnet_x = df,
-     rnet_y = network_segmented |> select(segment),
-     length_y = FALSE,
-     key_column = "df_id",
-     dist = join_dist
+  df_network_match = rnet_join(
+    rnet_x = df,
+    rnet_y = network_segmented |>
+      select(segment),
+    length_y = FALSE,
+    key_column = "df_id",
+    dist = join_dist
   ) |> st_drop_geometry()
 
-  df_network_attr = df_network_match %>%
-    left_join(df %>% st_drop_geometry() %>% select(attr, df_id), by="df_id")
+  df_network_attr = df_network_match |>
+    left_join(df |>
+                st_drop_geometry() |>
+                select(attr, df_id),
+              by = "df_id")
 
-  # 2. Group attr by segment
-  df_network_segment = df_network_attr %>% select(segment, attr) %>% group_by(segment) %>% summarise(!!attr:=fun(frequency))
+  # 3. Group attr by segment
+  df_network_segment = df_network_attr |>
+    select(segment, attr) |>
+    group_by(segment) |>
+    summarise(!!attr := fun(frequency))
 
-  # 3. Get geometry back
-  result = network_segmented %>%
-    filter(segment %in% df_network_segment$segment) %>%
-    left_join(df_network_segment, by="segment")
+  # 4. Get geometry back
+  result = network_segmented |>
+    filter(segment %in% df_network_segment$segment) |>
+    left_join(df_network_segment, by = "segment")
 
   return(result)
 }
