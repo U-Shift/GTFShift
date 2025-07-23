cd52a63399a61a06e9b970fe966ba3f308d220b7
Gonçalo Matos <goncaloafmatos@tecnico.ulisboa.pt>
2025-07-23 21:42:55 +0100

    #27 osm_routes_add_shapes.py improved to log output to file

--- inst/python/osm_routes_add_shapes.py
+++ inst/python/osm_routes_add_shapes.py
@@ -9,8 +9,22 @@ import osmapi
 from dotenv import load_dotenv, find_dotenv
 import os
 import pandas as pd
+import logging
 
 load_dotenv(find_dotenv())
+
+# Logging set up
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.DEBUG)  # Set the base logging level
+file_handler = logging.FileHandler('../../releases/osm_routes_add_shapes.log')
+file_handler.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 # Credentials you get from registering a new application
 # register here: https://master.apis.dev.openstreetmap.org/oauth2/applications
@@ -77,41 +91,53 @@ df = df[(df['distance_diff'] < 1000) & (df['points_diff'] < 500)] # Filter to on
 # Create change set, updating relations with tag gtfs:shape_id
 # The changeset comment can be customized to better describe the change submitted 
 with api.Changeset({"comment": "GTFS shapes association", "review_requested": "no", "locale": "pt", "source": "local knowledge"}) as changeset_id:
+  logger.info(f"Running changeset {changeset_id} for {len(df)} relations")
+  logger.info(f"{'route_id':20s}{'shape_id':20s} | {'osm_id':20s} | {'osm_route_id':20s}{'updated?':10s}{'osm_shape_id':20s}{'updated?':10s} | {'operation status':20s}")
   for idx, row in df.iterrows():
     route_id = row["route_id"] if "route_id" in row else None
     shape_id = row["shape_id"]
     osm_id = int(row["osm_id"])
+    
     relation = api.RelationGet(osm_id)
-    print(f"\tosm_id {osm_id} shape_id {shape_id} (previous {relation['tag']['gtfs:shape_id'] if 'gtfs:shape_id' in relation['tag'] else '-'})", end="\t")
-    if "gtfs:shape_id" not in relation["tag"] or relation["tag"]["gtfs:shape_id"] != shape_id or route_id and ("gtfs:route_id" not in relation["tag"] or relation["tag"]["gtfs:route_id"] != route_id):
+    relation_shape = relation["tag"]["gtfs:shape_id"] if "gtfs:shape_id" in relation["tag"] else "-"
+    relation_route = relation["tag"]["gtfs:route_id"] if "gtfs:route_id" in relation["tag"] else "-"
+    
+    status = "-"
+    if relation_shape != shape_id or route_id and (relation_route != route_id):
       relation["tag"]["gtfs:shape_id"] = shape_id # https://wiki.openstreetmap.org/wiki/Key:gtfs:shape_id
       if route_id:
         relation["tag"]["gtfs:route_id"] = route_id # https://wiki.openstreetmap.org/wiki/Key:gtfs:route_id
       update = api.RelationUpdate(relation)
-      print("Updated!")
+      status = "Updated"
     else:
-      print("Skipped...")
-  
+      status = "Skipped"
+    
+    logger.info(f"{route_id:20s}{shape_id:20s} | {osm_id:20d} | {relation_route:20s}{('⬜' if relation_route==route_id else '✏️'):10s}{relation_shape:20s}{('⬜' if relation_shape==shape_id else '✏️'):10s} | {status:20s}")
+
 # Rollback AVOID THIS! It should be used only if you made a mistake in the previous update and you want to rollback your changes
 # with api.Changeset({"comment": "GTFS shapes association rollback", "review_requested": "no", "locale": "pt", "source": "local knowledge"}) as changeset_id:
 #   for idx, row in df.iterrows():
 #     osm_id = int(row["osm_id"])
 #     relation = api.RelationGet(osm_id)
 #     relation_prev = api.RelationGet(osm_id, RelationVersion=relation["version"]-1)
-#     print("\t", osm_id, relation["tag"]["gtfs:shape_id"] if "gtfs:shape_id" in relation["tag"] else "-", "Current", relation["version"], "Previous", relation_prev["version"])
+#     logger.info(f"{osm_id} {relation['tag']['gtfs:shape_id'] if 'gtfs:shape_id' in relation['tag'] else '-'} Current {relation['version']} Previous {relation_prev['version']}")
 #     relation_prev["version"] = relation["version"] # We need to set version to last to enable update
 #     update = api.RelationUpdate(relation_prev)
 
 # Validate changes
-print(f"\t{'shape_id':20s}{'osm_id':20s}{'relation_shape==shape_id':10s}")
+logger.info("Validating changes...")
+logger.info(f"{'route_id':20s}{'shape_id':20s} | {'osm_id':20s} | {'osm_route_id':20s}{'osm_route_id==route_id':10s}{'osm_shape_id':20s}{'osm_shape_id==shape_id':10s}")
 for idx, row in df.iterrows():
     shape_id = row["shape_id"]
+    route_id = row["route_id"] if "route_id" in row else None
     osm_id = int(row["osm_id"])
     
     relation = api.RelationGet(osm_id)
   
     relation_shape = relation["tag"]["gtfs:shape_id"] if "gtfs:shape_id" in relation["tag"] else "-"
+    relation_route = relation["tag"]["gtfs:route_id"] if "gtfs:route_id" in relation["tag"] else "-"
   
-    if relation_shape!=shape_id:
-      print(f"\t{shape_id:20s}{osm_id:20d}{relation_shape==shape_id:10b}")
+    if relation_shape!=shape_id or relation_route!=route_id: 
+      logger.info(f"{route_id:20s}{shape_id:20s} | {osm_id:20d} | {relation_route:20s}{('✅' if relation_route==route_id else '❌'):10s}{relation_shape:20s}{('✅' if relation_shape==shape_id else '❌'):10s}")
+logger.info("Validation finished!")
 
