5ed5dd1626cfea13dc885cf8c49738e5c9ba50eb
goncaloafmatos <goncaloafmatos@tecnico.ulisboa.pt>
2025-11-05 14:02:40 +0000

    #32 network extension method improved with direction_wise and route_identifier parameters

--- R/get_network_extension.R
+++ R/get_network_extension.R
@@ -3,6 +3,8 @@
 #' Get total extension of GTFS feed routes
 #'
 #' @param gtfs tidygtfs. GTFS feed.
+#' @param route_identifier. String. (Default \code{"route_id"}). Column name in \code{gtfs$routes} identifying each route.
+#' @param direction_wise Boolean (Default \code{TRUE}). If TRUE, extension considers sum of both directions. Otherwise, only one direction is considered.
 #' @param date Date (Default \code{GTFShift::calendar_nextBusinessWednesday()}). Reference date to consider when analyzing the GTFS file.
 #' @param use_osm_routes osmdata::opq (Default NA). If overpass query for transit network is defined, analysis is performed considering OSM route geometry, using \code{GTFShift::osm_shapes_to_routes}.
 #' @param overline Boolean (Default FALSE). If TRUE, routes are aggregated using \code{stplanr::overline2()}, overlapping lines and converting them into a single route network.
@@ -26,7 +28,19 @@
 #' @import sf
 #'
 #' @export
-get_network_extension <- function(gtfs, date = GTFShift::calendar_nextBusinessWednesday(), use_osm_routes = NA, overline = FALSE) {
+get_network_extension <- function(
+    gtfs,
+    route_identifier = "route_id",
+    direction_wise = TRUE,
+    date = GTFShift::calendar_nextBusinessWednesday(),
+    use_osm_routes = NA,
+    overline = FALSE
+) {
+
+  # Validate if route_identifier exists
+  if (!(route_identifier %in% colnames(gtfs$routes))) {
+    stop("route_identifier should be one of gtfs$routes columns")
+  }
 
   # Compute hourly frequencies for each route
   network = gtfs |> GTFShift::get_route_frequency_hourly(date = date, use_osm_routes = use_osm_routes, overline = overline)
@@ -39,20 +53,25 @@ get_network_extension <- function(gtfs, date = GTFShift::calendar_nextBusinessWe
   # Compute daily frequencies per route shape
   network_redux = network |>
     st_drop_geometry() |>
-    group_by(route_short_name, route_id, direction_id, shape_id) |>
+    group_by(.data[[route_identifier]], direction_id, shape_id) |>
     summarise(frequency_day = sum(frequency)) |>
     ungroup()
 
   # Get shape with max frequencies per route
   network_redux_max = network_redux |>
-    group_by(route_short_name, direction_id) |>
+    # Get max frequency shape per route (and direction, if direction_wise=TRUE)
+    group_by(.data[[route_identifier]], shape_id, !!!if (direction_wise) rlang::syms("direction_id")) |>
     summarise(frequency_max = max(frequency_day)) |>
+    # Get shape with max frequency per route (and direction, if direction_wise=TRUE)
+    group_by(
+      .data[[route_identifier]],
+      !!!if (direction_wise) rlang::syms("direction_id")
+    ) |>
+    slice_max(order_by = frequency_max, n = 1, with_ties = FALSE) |>
     ungroup()
 
   # Join with the original network to get the shapes and compute its distance
   network_redux_shapes = network_redux_max |>
-    left_join(network_redux |> select(-route_id), by = c("route_short_name", "direction_id", "frequency_max" = "frequency_day")) |>
-    select(-frequency_max) |>
     left_join(shapes_unique, by = "shape_id") |>
     st_as_sf() |>
     st_transform(crs = 3857) |> # For units in meters
--- dev/test_get_network_extension.R
+++ dev/test_get_network_extension.R
@@ -2,29 +2,31 @@ library(GTFShift)
 library(dplyr)
 library(units)
 
+# https://github.com/U-Shift/GTFShift/issues/32
+
 # Carris, Lisboa
 # Expected 1849km of bus, 67km of tram (https://www.carris.pt/media/lwsixqty/relatorio-e-contas-2023.pdf)
 gtfs_carris = load_feed("https://files.mobilitydatabase.org/mdb-1032/mdb-1032-202505170214/mdb-1032-202505170214.zip", create_transfers = FALSE)
 summary(gtfs_carris)
 
-route_extent_carris = get_network_extension(gtfs_carris, date="2025-05-21")
-drop_units(route_extent_carris/1000) # CRS4326: 1937.284 // CRS3857: 2486.455 km
+route_extent_carris = get_network_extension(gtfs_carris, date="2025-05-21", route_identifier="route_short_name")
+drop_units(route_extent_carris/1000) # 2486.455 km
 
 bus_routes = gtfs_carris$routes |> filter(!grepl("E", route_short_name)) |> left_join(gtfs_carris$trips)
 gtfs_carris_bus = tidytransit::filter_feed_by_trips(gtfs_carris, trip_ids = bus_routes$trip_id)
 summary(gtfs_carris_bus)
 length(unique(gtfs_carris_bus$routes$route_short_name))
 
-route_extent_carris_bus = get_network_extension(gtfs_carris_bus, date="2025-05-21")
-drop_units(route_extent_carris_bus/1000) # CRS4326: 1880.159 // CRS3857: 2413.167 km
+route_extent_carris_bus = get_network_extension(gtfs_carris_bus, date="2025-05-21", route_identifier="route_short_name", direction_wise=TRUE)
+drop_units(route_extent_carris_bus/1000) # 2413.167 km
 
 tram_routes = gtfs_carris$routes |> filter(grepl("E", route_short_name) & !grepl("Ascensor", route_long_name) & !grepl("Elevador", route_long_name)) |> left_join(gtfs_carris$trips)
 gtfs_carris_tram = tidytransit::filter_feed_by_trips(gtfs_carris, trip_ids = tram_routes$trip_id)
 summary(gtfs_carris_tram)
 length(unique(gtfs_carris_tram$routes$route_short_name))
 
-route_extent_carris_tram = get_network_extension(gtfs_carris_tram, date="2025-05-21")
-drop_units(route_extent_carris_tram/1000) # CRS4326: 55.71755 // CRS3857: 71.48166 km
+route_extent_carris_tram = get_network_extension(gtfs_carris_tram, date="2025-05-21", route_identifier="route_short_name")
+drop_units(route_extent_carris_tram/1000) # 71.48166 km
 
 # Metro Lisboa
 # Expected 44.5km (https://pt.wikipedia.org/wiki/Metropolitano_de_Lisboa)
@@ -32,7 +34,20 @@ gtfs_ml = GTFShift::load_feed("https://files.mobilitydatabase.org/tld-716/tld-71
 summary(gtfs_ml)
 
 route_extent_ml = get_network_extension(gtfs_ml, date="2025-05-21")
-drop_units(route_extent_ml/1000) # CRS4326: 90.99035 // CRS3857: 116.7961 km
+drop_units(route_extent_ml/1000) # 116.7961 km
 
+route_extent_ml_single = get_network_extension(gtfs_ml, date="2025-05-21", direction_wise=FALSE)
+drop_units(route_extent_ml_single/1000) # 51.77 km
 
+# STCP
+# Expected 503 km (https://stcp.pt/pt/stcp-em-numeros)
+gtfs_stcp = load_feed("https://opendata.porto.digital/dataset/5275c986-592c-43f5-8f87-aabbd4e4f3a4/resource/89a6854f-2ea3-4ba0-8d2f-6558a9df2a98/download/horarios_gtfs_stcp_16_04_2025.zip", create_transfers = FALSE)
+summary(gtfs_stcp)
 
+length(unique(gtfs_stcp$routes$route_id))
+
+route_extent_stcp_2ways = get_network_extension(gtfs_stcp, date="2025-11-05")
+drop_units(route_extent_stcp_2ways/1000) # 2398.016 km
+
+route_extent_stcp_1way = get_network_extension(gtfs_stcp, date="2025-11-05", direction_wise = FALSE)
+drop_units(route_extent_stcp_1way/1000) # 1241.562 km
