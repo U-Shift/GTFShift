50bf3f9460b2256f1511f424c44c8d7f439f58f5
gmatosferreira <goncaloafmatos@tecnico.ulisboa.pt>
2026-05-12 12:34:41 +0100

    #75 rt_extend_prioritization docs extended to include internal filter that was implemented but not disclosed

--- R/rt_extend_prioritization.R
+++ R/rt_extend_prioritization.R
@@ -9,6 +9,8 @@
 #' @details
 #' Extends the \code{lane_prioritization} data with speed metrics calculated from the GTFS-RT data points that fall within a buffer around each lane segment.
 #'
+#' If the \code{current_status} column is present in the \code{rt_collection} data, only points with \code{current_status == "IN_TRANSIT_TO"} are considered.
+#'
 #' Refer to \code{GTFShift::rt_collect_json()} or \code{GTFShift::rt_collect_protobuf()} for details on GTFS-RT data collection.
 #'
 #'
@@ -45,14 +47,14 @@ rt_extend_prioritization <- function(
   lane_buffer = 15 # in meters
 ) {
   # 1. Validate inputs
-  required_cols = c("way_osm_id")
-  missing_cols = setdiff(required_cols, colnames(lane_prioritization))
+  required_cols <- c("way_osm_id")
+  missing_cols <- setdiff(required_cols, colnames(lane_prioritization))
   if (length(missing_cols) > 0) {
     stop(paste("lane_prioritization is missing required columns:", paste(missing_cols, collapse = ", ")))
   }
-  rt_attr_speed = "speed"
-  required_rt_cols = c(rt_attr_speed)
-  missing_rt_cols = setdiff(required_rt_cols, colnames(rt_collection))
+  rt_attr_speed <- "speed"
+  required_rt_cols <- c(rt_attr_speed)
+  missing_rt_cols <- setdiff(required_rt_cols, colnames(rt_collection))
   if (length(missing_rt_cols) > 0) {
     stop(paste("rt_collection is missing required columns:", paste(missing_rt_cols, collapse = ", ")))
   }
@@ -60,7 +62,7 @@ rt_extend_prioritization <- function(
   # Display feedback
   pb <- progress::progress_bar$new( # Track progress
     format = "Extending prioritization with GTFS-RT metrics [:bar] :percent :spin elapsed=:elapsed",
-    clear = FALSE, show_after=0
+    clear = FALSE, show_after = 0
   )
   pb$update(0)
 
@@ -75,51 +77,63 @@ rt_extend_prioritization <- function(
   job <- callr::r_bg(function(lane_prioritization) { # update spinner while blocking method call
     library(sf)
     return(lane_prioritization |>
-            dplyr::distinct(way_osm_id, .keep_all = TRUE) |>
-            dplyr::select(way_osm_id))
-  }, args=list(lane_prioritization))
-  while (job$is_alive()) { pb$tick(0); Sys.sleep(0.1) }
+      dplyr::distinct(way_osm_id, .keep_all = TRUE) |>
+      dplyr::select(way_osm_id))
+  }, args = list(lane_prioritization))
+  while (job$is_alive()) {
+    pb$tick(0)
+    Sys.sleep(0.1)
+  }
   lanes_unique <- job$get_result()
   pb$update(0.333)
 
   # 4. Create buffers in lane segments to overlap with updates
   job <- callr::r_bg(function(lanes_unique, lane_buffer) { # update spinner while blocking method call
     return(sf::st_buffer(
-            sf::st_transform(lanes_unique, crs=3857),
-            dist=lane_buffer
-          ) |> sf::st_transform(crs=sf::st_crs(lanes_unique)))
-  }, args=list(lanes_unique, lane_buffer))
-  while (job$is_alive()) { pb$tick(0); Sys.sleep(0.1) }
+      sf::st_transform(lanes_unique, crs = 3857),
+      dist = lane_buffer
+    ) |> sf::st_transform(crs = sf::st_crs(lanes_unique)))
+  }, args = list(lanes_unique, lane_buffer))
+  while (job$is_alive()) {
+    pb$tick(0)
+    Sys.sleep(0.1)
+  }
   lane_buffers <- job$get_result()
   pb$update(0.5)
 
   # 4. Spatial join between lane buffers and rt_collection points
   job <- callr::r_bg(function(rt_collection, lane_buffers) { # update spinner while blocking method call
     return(sf::st_join(
-            rt_collection,
-            lane_buffers |> dplyr::select(way_osm_id),
-            left = FALSE,
-            join = sf::st_within
-          ) |> sf::st_drop_geometry())
-  }, args=list(rt_collection, lane_buffers))
-  while (job$is_alive()) { pb$tick(0); Sys.sleep(0.1) }
+      rt_collection,
+      lane_buffers |> dplyr::select(way_osm_id),
+      left = FALSE,
+      join = sf::st_within
+    ) |> sf::st_drop_geometry())
+  }, args = list(rt_collection, lane_buffers))
+  while (job$is_alive()) {
+    pb$tick(0)
+    Sys.sleep(0.1)
+  }
   overlap <- job$get_result()
   pb$update(0.666)
 
   # 5. Aggregate speed metrics by way_osm_id
   job <- callr::r_bg(function(overlap, rt_attr_speed) { # update spinner while blocking method call
     return(overlap |>
-             dplyr::group_by(way_osm_id) |>
-             dplyr::summarise(
-               speed_avg = mean(.data[[rt_attr_speed]], na.rm = TRUE),
-               speed_median = stats::median(.data[[rt_attr_speed]], na.rm = TRUE),
-               speed_p25 = stats::quantile(.data[[rt_attr_speed]], probs = 0.25, na.rm = TRUE),
-               speed_p75 = stats::quantile(.data[[rt_attr_speed]], probs = 0.75, na.rm = TRUE),
-               speed_count = dplyr::n()
-             ) |>
-             dplyr::ungroup())
-  }, args=list(overlap, rt_attr_speed))
-  while (job$is_alive()) { pb$tick(0); Sys.sleep(0.1) }
+      dplyr::group_by(way_osm_id) |>
+      dplyr::summarise(
+        speed_avg = mean(.data[[rt_attr_speed]], na.rm = TRUE),
+        speed_median = stats::median(.data[[rt_attr_speed]], na.rm = TRUE),
+        speed_p25 = stats::quantile(.data[[rt_attr_speed]], probs = 0.25, na.rm = TRUE),
+        speed_p75 = stats::quantile(.data[[rt_attr_speed]], probs = 0.75, na.rm = TRUE),
+        speed_count = dplyr::n()
+      ) |>
+      dplyr::ungroup())
+  }, args = list(overlap, rt_attr_speed))
+  while (job$is_alive()) {
+    pb$tick(0)
+    Sys.sleep(0.1)
+  }
   speed_metrics <- job$get_result()
   pb$update(0.833)
 
@@ -127,9 +141,12 @@ rt_extend_prioritization <- function(
   job <- callr::r_bg(function(lane_prioritization, speed_metrics) { # update spinner while blocking method call
     library(sf)
     return(lane_prioritization |>
-             dplyr::left_join(speed_metrics, by = "way_osm_id"))
-  }, args=list(lane_prioritization, speed_metrics))
-  while (job$is_alive()) { pb$tick(0); Sys.sleep(0.1) }
+      dplyr::left_join(speed_metrics, by = "way_osm_id"))
+  }, args = list(lane_prioritization, speed_metrics))
+  while (job$is_alive()) {
+    pb$tick(0)
+    Sys.sleep(0.1)
+  }
   lane_prioritization_extended <- job$get_result()
   pb$update(1)
   pb$terminate()
