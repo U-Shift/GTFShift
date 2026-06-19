fb4a6b318d88d2b42dd1e071344262bd7a09f341
gmatosferreira <goncaloafmatos@tecnico.ulisboa.pt>
2026-06-19 17:39:30 +0100

    #84 osm_shapes_match_routes railway validation for CP Portugal Lisbon services

--- dev/test_match_rail.R
+++ dev/test_match_rail.R
@@ -7,7 +7,7 @@ gtfs$shapes
 
 
 # https://github.com/lxparapessoas/rede-madrugada/blob/main/analysis.md
-stations_lisbon = c(
+stations_lisbon_u = c(
   "94_61101", # Sintra
   "94_62042", # Meleças
   "94_59006", # Rossio
@@ -22,8 +22,14 @@ stations_lisbon = c(
   "94_95000", # Barreiro
   "94_91058" # Praias do Sado A
 )
+stations_lisbon_r = c(
+  "94_40154" # Tomar
+)
 routes_lisbon = gtfs$routes %>%
-  filter(grepl(paste(stations_lisbon, collapse = "|"), route_id) & (route_short_name == "U" | grepl("^Linha", route_short_name)))
+  filter(
+    grepl(paste(stations_lisbon_u, collapse = "|"), route_id) & (route_short_name == "U" | grepl("^Linha", route_short_name))
+    | grepl(paste(stations_lisbon_r, collapse = "|"), route_id) & (route_short_name %in% c("R", "IR"))
+  )
 trips_lisbon = gtfs$trips %>%
   filter(route_id %in% routes_lisbon$route_id)
 gtfs = tidytransit::filter_feed_by_trips(gtfs, trip_ids = trips_lisbon$trip_id) 
@@ -35,6 +41,7 @@ gtfs = GTFShift::create_shapes_from_stops(gtfs)
 gtfs_shapes_sf <- tidytransit::shapes_as_sf(gtfs$shapes)
 mapview(gtfs_shapes_sf)
 
+library(stringr)
 manipulate_gtfs_cp <- function(gtfs) {
   # Method to manipulate GTFS routes names, to enable match with OSM names
   # See https://github.com/U-Shift/GTFShift/issues/35 for more details
@@ -62,16 +69,38 @@ manipulate_gtfs_cp <- function(gtfs) {
 gtfs <- manipulate_gtfs_cp(gtfs)
 
 # Match shapes
+library(osmdata)
 bbox <- sf::st_bbox(gtfs_shapes_sf)
 q <- opq(bbox = bbox) |>
   add_osm_feature(key = "route", value = "train", key_exact = TRUE) |>
   add_osm_feature(key = "operator", value = "Comboios de Portugal", key_exact = TRUE)
   
-osm_match <- GTFShift::osm_shapes_match_routes(
+osm_file <- osmextract::oe_download(
+  sprintf("https://download.geofabrik.de/%s-latest.osm.pbf", "europe/portugal"),
+  file_basename = sprintf("%s_%s.osm.pbf", str_replace_all("europe/portugal", "/", "_"), format(Sys.Date(), "%Y%m%d"))
+)
+
+osm_match_result <- GTFShift::osm_shapes_match_routes(
   gtfs, 
   q,
   gtfs_match = "route_short_name",
   osm_match = "name",
   gtfs_osm_match_exact = FALSE,
-  osm_route_type = "train"
+  osm_route_type = "train",
+  osm_file = osm_file
 )
+
+osm_match_result_unique_routes <- osm_match_result |>
+  select(-trip_short_name) |>
+  distinct()
+osm_match_result_unique_routes
+
+mapview(osm_match_result_unique_routes, zcol="osm_name")
+mapview(osm_match_result_unique_routes, zcol="route_short_name")
+
+
+length(unique(routes_lisbon$route_id))
+length(unique(osm_match_result_unique_routes$route_id))
+length(unique(osm_match_result_unique_routes$route_id)) / length(unique(routes_lisbon$route_id))
+
+View(routes_lisbon |> filter(!route_id %in% unique(osm_match_result_unique_routes$route_id)))
